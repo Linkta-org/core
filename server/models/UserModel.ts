0853fd0fd2b02176dd869131f7542bb21deb1d6b
Lucienne Seidler <lucienneseidler@luciennes-mbp.lan>
2024-03-19 15:45:45 -0400

    updated user model to reflect naming practices and set default empty array for treeId

--- server/models/UserModel.ts
+++ server/models/UserModel.ts
@@ -2,13 +2,13 @@ import { Schema, model, Types } from "mongoose";
 
 interface User {
   _id: Types.ObjectId;
-  userId: string;
+  firebaseId: string;
   treeIds: [string];
 }
 const userSchema = new Schema<User>({
   _id: { required: true, unique: true },
-  userId: { type: String, required: true, unique: true },
-  treeIds: { type: [String], required: true },
+  firebaseId: { type: String, required: true, unique: true },
+  treeIds: { type: [String], default: [] },
 });
 
 module.exports = model("User", userSchema);

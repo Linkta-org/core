ed95f19a39df4544a585e789662e497c3e33960a
Lucienne Seidler <lucienneseidler@luciennes-mbp.lan>
2024-03-21 11:15:04 -0400

    merged incoming changes

--- server/models/UserModel.ts
+++ server/models/UserModel.ts
@@ -1,9 +1,9 @@
-import { Schema, model, Types } from "mongoose";
-import { User } from "../types/datamodels";
+import { Schema, model, Types } from 'mongoose';
+import { User } from '../types/datamodels';
 
 const userSchema = new Schema<User>({
   firebaseId: { type: String, required: true, unique: true },
   treeIds: { type: [Types.ObjectId], default: [] },
 });
 
-module.exports = model("User", userSchema);
+module.exports = model('User', userSchema);
--- server/types/datamodels.d.ts
+++ server/types/datamodels.d.ts
@@ -1,4 +1,4 @@
-import { Schema, Types } from "mongoose";
+import { Types } from "mongoose";
 
 export type User = {
   _id: Types.ObjectId;

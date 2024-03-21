b29afe5c26e0eec1885a82caf2a75b728a4fef2a
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-20 23:41:52 -0500

    add comment

--- client/components/layout/main-layout/MainLayout.tsx
+++ client/components/layout/main-layout/MainLayout.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 import { Outlet } from 'react-router-dom';
 import Header from './header/Header';
 import Footer from './footer/Footer';
-
+/**
+ * MainLayout component wraps the children components with a common structure
+ * It is used to provide a consistent layout across different pages.
+ * @param props - The component props.
+ * @param props.showFooter - Determines if the footer should be rendered. Defaults to false.
+ * @returns The MainLayout component with Header, Outlet for nested routes, and optional Footer.
+ */
 interface MainLayoutProps {
   showFooter?: boolean;
 }

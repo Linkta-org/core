3cc02911c24c1a3bd94b287977931c0415038e51
philzmintmojito <140580833+philzmintmojito@users.noreply.github.com>
2024-03-22 11:21:43 -0700

    fix: remove redix UI from main.jsx

--- client/main.jsx
+++ client/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Theme } from '@radix-ui/themes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Theme>
-      <App />
-    </Theme>
+
+    <App />
+
   </React.StrictMode>
 )

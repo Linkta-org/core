d8da06dee811742618add5a955a02648d0f56743
philzmintmojito <140580833+philzmintmojito@users.noreply.github.com>
2024-03-22 11:25:14 -0700

    fix: remove redix and footer from App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
-import '@radix-ui/themes/styles.css';
-import Footer from "./components/layout/main-layout/footer/Footer";
+
 export default function App() {
   return (
-    <>
-      <h1>Linktaaaaaaaa</h1>
-      <div id="footer">
-        <Footer />
-      </div>
-    </>
+    <h1>Linktaaaaaaaa</h1>
   )
 }

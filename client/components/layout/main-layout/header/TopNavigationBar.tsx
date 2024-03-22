37f19baeb6b755712a6e0077854f56bd6fde97f6
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-21 22:08:34 -0500

    add comment

--- client/components/layout/main-layout/header/TopNavigationBar.tsx
+++ client/components/layout/main-layout/header/TopNavigationBar.tsx
@@ -3,7 +3,11 @@ import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 import { Link, useLocation } from 'react-router-dom';
 import { topNavigationTabs } from '../layoutConfig';
 import { Tab } from '../../../../types';
-
+/**
+ * Displays navigation links dynamically based on the current route.
+ *
+ * Note: The `pathname` from `useLocation` is used to determine the current route
+ */
 const TopNavigationBar: React.FC = () => {
   const { pathname } = useLocation();
   const currentTabs: Tab[] =

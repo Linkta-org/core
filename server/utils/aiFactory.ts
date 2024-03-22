18a4010553ec6c4f30ea66cda15e555c80520c7d
Darren Pavel <darren.pavel@gmail.com>
2024-03-22 12:40:16 -0700

    Clean up the commit to only include necessary changes.
    Pull creating a Gemini instance into a function for generalization.

--- server/utils/aiFactory.ts
+++ server/utils/aiFactory.ts
@@ -1,6 +1,7 @@
 import Gemini from '@/server/models/GeminiModel';
 
 import type { AiInterface, AiTypes } from '@/server/types/ai';
+import { func } from 'prop-types';
 
 /**
  * AI Factory interface.
@@ -11,8 +12,19 @@ import type { AiInterface, AiTypes } from '@/server/types/ai';
 export default function createAi(ai: AiTypes): AiInterface {
   switch (ai) {
     case 'gemini':
-      return new Gemini();
+      return setupGemini();
     default:
       throw new Error('Invalid AI type');
   }
+}
+
+/**
+ * Function to setup and return Gemini AI.
+ * This seems redundant since it only returns a new object, but is created for consistency
+ * with potential future AI connections that require more setup.
+ *
+ * @return Gemini AI
+ */
+function setupGemini(): AiInterface {
+  return new Gemini();
 }

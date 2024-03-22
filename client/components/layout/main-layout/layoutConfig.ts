89d1cdae6cefbb6f6d4c9ee10b421220dd25db91
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-21 21:35:22 -0500

    add comment

--- client/components/layout/main-layout/layoutConfig.ts
+++ client/components/layout/main-layout/layoutConfig.ts
@@ -1,6 +1,8 @@
+//todo: add more paths & corresponding tabs
 export const topNavigationTabs = {
   '/': [
     { tabname: 'Sign In', path: '/sign-in' },
     { tabname: 'Start Growing', path: '/sign-up' },
+    { tabname: 'Explore', path: '/generate' },
   ],
 };

68886a519727f4bb2c2342701f2405ebf40da2ee
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-21 21:19:16 -0500

    implement topnavbar

--- client/components/layout/main-layout/header/TopNavigationBar.tsx
+++ client/components/layout/main-layout/header/TopNavigationBar.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+import { Link, useLocation } from 'react-router-dom';
+import { topNavigationTabs } from '../layoutConfig';
 
 const TopNavigationBar = () => {
-  return <div>TopNavigationBar Placeholder</div>;
+  const location = useLocation();
+  const currentTabs =
+    topNavigationTabs[location.pathname] || topNavigationTabs['/'];
+
+  return (
+    <NavigationMenu.Root>
+      <NavigationMenu.List>
+        {currentTabs &&
+          currentTabs.map((tab) => (
+            <NavigationMenu.Item key={tab.tabname}>
+              <Link to={tab.path}>{tab.tabname}</Link>
+            </NavigationMenu.Item>
+          ))}
+      </NavigationMenu.List>
+    </NavigationMenu.Root>
+  );
 };
 
 export default TopNavigationBar;

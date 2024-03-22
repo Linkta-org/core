9a55da1d156c9e7db5fb054f6064b4f1d05b7305
Andrew Lo <andrewlo@Andrews-MBP.lan>
2024-03-22 10:23:10 -0700

    created ErrorPage component with code from NotFoundPage

--- client/main.tsx
+++ client/main.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import NotFoundPage from './features/not-found-page/NotFoundPage';
 import MainLayout from './components/layout/main-layout/MainLayout';
 import PromptInputPage from './features/prompt-input-page/PromptInputPage';
 import OutputVisualizationPage from './features/output-visualization-page/OutputVisualizationPage';
+import ErrorPage from './features/error-pages/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    errorElement: <NotFoundPage />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/generate", element: <PromptInputPage /> },
       { path: "/tree", element: <OutputVisualizationPage />},

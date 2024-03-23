e7bf2ef32573ac28df5be6554e3b9882bfe37254
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-23 11:15:36 -0500

    update root path for vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname),
 });

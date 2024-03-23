3e0405a6172ad2bc3e8de86ea9092f08e2303aca
ejwoods <ewoods08@gmail.com>
2024-03-23 17:41:09 -0400

    add further comments to navigationStore

--- client/store/navigationStore.ts
+++ client/store/navigationStore.ts
@@ -15,6 +15,8 @@ const useNavigationStore = create<NavigationState>((set) => ({
 }));
 
 // export only custom hooks so that components cannot subscribe to the entire store
+// atomic selectors: selectors target smallest possible values when possible
 export const useCurrentPage = () => useNavigationStore((state) => state.currentPage)
 
+// separate actions from state - actions object can be considered atomic because its values won't change
 export const useNavigationActions = () => useNavigationStore((state) => state.actions)

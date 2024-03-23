684c97bc68d7dd81b3c3d40e77c95105c73e88c0
ejwoods <ewoods08@gmail.com>
2024-03-23 17:26:39 -0400

    feat: Add navigationStore demo (sans react router) to App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { useCurrentPage, useNavigationActions } from './store/navigationStore.ts'
 
 export default function App() {
-  return <h1>Linktaaaaaaaa</h1>;
+  // subscribe App to currentPage slice of store
+  const currentPage = useCurrentPage();
+
+  const navigationActions = useNavigationActions();
+
+  const handleClick = (page) => {
+    return navigationActions.setCurrentPage(page);
+  }
+  
+  return (
+    <>
+      <h1>Linkta</h1>
+      <p>Current Page: {currentPage}</p>
+      <button onClick={() => handleClick('Page 1')}>Page 1</button>
+      <button onClick={() => handleClick('Page 2')}>Page 2</button>
+    </>
+  )
 }

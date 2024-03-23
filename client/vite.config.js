cc2006884d85e6636455207574e1217e910d3ec7
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-23 12:21:57 -0500

    add tsconfigpaths plugin in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react()],
-  root: path.resolve(__dirname),
+  plugins: [
+    react(), // React plugin for Vite
+    tsconfigPaths(), // Resolve imports using TypeScript's path mapping
+  ],
+  root: path.resolve(__dirname), // Define root directory for the Vite project.
   server: {
     proxy: {
-      '/api': 'http://localhost:3000'
-    }
+      // Proxy configuration for API calls
+      '/api': 'http://localhost:3000',
+    },
   },
 });

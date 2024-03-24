d194562ece6dcd554e09e1f4757f1467f1491812
lomeintheory <lomeintheory@users.noreply.github.com>
2024-03-24 22:32:59 +0000

    chore: format files from GitHub Action

--- client/routes/privateRoutes.tsx
+++ client/routes/privateRoutes.tsx
@@ -1,4 +1,4 @@
-import { RouteObject } from 'react-router-dom';
+import type { RouteObject } from 'react-router-dom';
 
 const privateRoutes: RouteObject[] = [
   // currently left blank since no private routes have been defined in our project yet
--- client/routes/publicRoutes.tsx
+++ client/routes/publicRoutes.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import MainLayout from '../components/layout/main-layout/MainLayout';
-import PromptInputPage from '../features/prompt-input-page/PromptInputPage';
-import OutputVisualizationPage from '../features/output-visualization-page/OutputVisualizationPage';
-import { RouteObject } from 'react-router-dom';
-import NotFoundPage from '../features/not-found-page/NotFoundPage';
+import MainLayout from '@/client/components/layout/main-layout/MainLayout';
+import PromptInputPage from '@/client/features/prompt-input-page/PromptInputPage';
+import OutputVisualizationPage from '@/client/features/output-visualization-page/OutputVisualizationPage';
+import type { RouteObject } from 'react-router-dom';
+import NotFoundPage from '@/client/features/not-found-page/NotFoundPage';
 
 /**
- * The publicRoutes variable is an array which contains objects that each represent a route. 
- * Our path property is set to the default route. Setting the element property to the MainLayout 
- * component at the top level will allow its children to render MainLayout. 
- * 
+ * The publicRoutes variable is an array which contains objects that each represent a route.
+ * Our path property is set to the default route. Setting the element property to the MainLayout
+ * component at the top level will allow its children to render MainLayout.
+ *
  */
 
 const publicRoutes: RouteObject[] = [
   {
-    path: "/",
+    path: '/',
     element: <MainLayout />,
     children: [
-      { path: "/generate", element: <PromptInputPage /> },
-      { path: "/output", element: <OutputVisualizationPage />},
-    ]
+      { path: '/generate', element: <PromptInputPage /> },
+      { path: '/output', element: <OutputVisualizationPage /> },
+    ],
   },
   // here we implement a catch-all route handler that renders the NotFoundPage component for unknown routes
   {
-    path: "*",
-    element: <NotFoundPage />
-  }
+    path: '*',
+    element: <NotFoundPage />,
+  },
 ];
 
 export default publicRoutes;

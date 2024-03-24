9fc6716c53934a6ca5fa4f7651e87f3f425231b7
Andrew Lo <andrewlo957@gmail.com>
2024-03-24 15:25:00 -0700

    moved routing logic to App.tsx

--- client/main.tsx
+++ client/main.tsx
@@ -1,25 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import MainLayout from './components/layout/main-layout/MainLayout';
-import PromptInputPage from './features/prompt-input-page/PromptInputPage';
-import OutputVisualizationPage from './features/output-visualization-page/OutputVisualizationPage';
-import ErrorPage from './features/error-pages/ErrorPage';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      { path: "/generate", element: <PromptInputPage /> },
-      { path: "/tree", element: <OutputVisualizationPage />},
-    ]
-  },
-]);
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <App />
   </React.StrictMode>
 );

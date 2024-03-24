b2d8e79a72ce025cbb6a25fbc3b5d9483b6379f3
Andrew Lo <andrewlo957@gmail.com>
2024-03-24 15:30:26 -0700

    modularized routes

---
+++ client/routes/privateRoutes.tsx
@@ -0,0 +1,7 @@
+import { RouteObject } from 'react-router-dom';
+
+const privateRoutes: RouteObject[] = [
+  // currently left blank since no private routes have been defined in our project yet
+];
+
+export default privateRoutes;

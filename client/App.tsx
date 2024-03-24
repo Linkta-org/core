f9fc499781cac88011382197fe22d688261aee87
Andrew Lo <andrewlo957@gmail.com>
2024-03-24 15:25:00 -0700

    moved routing logic to App.tsx

--- client/App.tsx
+++ client/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/index';
 
 const App: React.FC = () => {
   return (
-    <h1>Helllooo</h1>
+    <RouterProvider 
+      router={router}
+    />
   )
 }
 

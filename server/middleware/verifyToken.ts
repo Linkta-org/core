cec0ef12dd9280cc2ceab015ba00cef918147be9
zhiyi15 <zhiyihuang1994@gmail.com>
2024-03-25 10:24:55 -0700

    commented out verifyToken middleware

--- server/middleware/verifyToken.ts
+++ server/middleware/verifyToken.ts
@@ -1,15 +1,15 @@
-import { NextFunction } from "express";
-import admin from "../config/firebaseAdmin";
+// import { NextFunction } from "express";
+// import admin from "../config/firebaseAdmin";
 
-const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
-  //TODO: figure how are we receiving JWT token from client side(in cookie? headers?)
-  const token = req;
-  try {
+// const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
+//   //TODO: figure how are we receiving JWT token from client side(in cookie? headers?)
+//   const token = req;
+//   try {
 
-  } catch (error) {
+//   } catch (error) {
     
-  }
+//   }
   
-}
+// }
 
-export default verifyToken;
+// export default verifyToken;

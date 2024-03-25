583678f0c7468dbb53e493635251585692ee1f30
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-24 20:18:40 -0500

    convert to react 18 preferred syntax

--- client/main.tsx
+++ client/main.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const rootElement = document.getElementById('root');
+
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>

e6c048aa73ef6cfe76a2535b7a2b16e0e1f48d92
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-24 21:33:32 -0500

    add comments

--- client/components/layout/main-layout/header/TopNavigationBar.tsx
+++ client/components/layout/main-layout/header/TopNavigationBar.tsx
@@ -3,11 +3,12 @@ import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 import { Link, useLocation } from 'react-router-dom';
 import { topNavigationTabsByRoute } from '@/client/components/layout/main-layout/layoutConfig';
 import type { Tab } from '@/client/types/layout';
-
 /**
- * Displays navigation links dynamically based on the current route.
+ * Renders a navigation menu with tabs based on the current route.
  *
- * Note: The `pathname` from `useLocation` is used to determine the current route
+ * Note:
+ * The `pathname` from `useLocation` is used to determine the current route.
+ * The `topNavigationTabsByRoute[pathname]` gets the tabs for the current route, fallback to '/' if not found.
  */
 const TopNavigationBar: React.FC = () => {
   const { pathname } = useLocation();
--- client/components/layout/main-layout/layoutConfig.ts
+++ client/components/layout/main-layout/layoutConfig.ts
@@ -1,11 +1,16 @@
 import type { Tab } from '@/client/types/layout';
-
+/**
+ * Tab objects representing different route.
+ */
 const homeTab = { tabname: 'Home', path: '/' };
 const signInTab = { tabname: 'Sign In', path: '/sign-in' };
 const signUpTab = { tabname: 'Start Growing', path: '/sign-up' };
 const exploreTab = { tabname: 'Explore', path: '/generate' };
 const visualizeTab = { tabname: 'Visualize', path: '/output' };
-
+/**
+ * Object mapping routes to their corresponding navigation tabs.
+ * Each key represents a route, and the value is an array of Tab objects.
+ */
 export const topNavigationTabsByRoute: Record<string, Tab[]> = {
   '/': [signInTab, signUpTab, exploreTab, visualizeTab],
   '/generate': [homeTab, visualizeTab],

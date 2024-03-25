655112d0e6f0faf3beeeda9209c26fcc409612dd
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-24 22:32:31 -0500

    add comment

--- client/hooks/useDynamicNavigation.ts
+++ client/hooks/useDynamicNavigation.ts
@@ -1,7 +1,12 @@
 import { useLocation } from 'react-router-dom';
 import { topNavigationTabsByRoute } from '@/client/components/layout/main-layout/layoutConfig';
 import type { Tab } from '@/client/types/layout';
-
+/**
+ * Custom hook for determining navigation bar visibility and fetching relevant tabs based on the current route.
+ *
+ * @returns { showTopNavBar, currentTabs } An object containing the visibility state of the top navigation bar
+ * and the tabs to display.
+ */
 const useDynamicNavigation = () => {
   const { pathname } = useLocation();
 

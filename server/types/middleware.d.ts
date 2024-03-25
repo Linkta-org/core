baf48dfef40b99452cb56d470a00c418d45da9db
Darren Pavel <darren.pavel@gmail.com>
2024-03-25 13:18:30 -0700

    🐞 fix(Fix Error Handling):
    Because express middleware routing looks for Errors, using the MiddlewareError type breaks error handling. This will properly handle errors.

--- server/types/middleware.d.ts
+++ server/types/middleware.d.ts
@@ -1,7 +1,10 @@
-export type MiddlewareError = {
+/**
+ * The Error object to be used in the error handling middleware.
+ */
+export interface MiddlewareError extends Error {
   log: string;
   status: number;
   message: {
     err: string;
   };
-};
+}

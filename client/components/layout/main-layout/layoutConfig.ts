c3380374a8e3c84b8ed6bfb4c6103d32e3043478
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-24 21:22:55 -0500

    add home and output

--- client/components/layout/main-layout/layoutConfig.ts
+++ client/components/layout/main-layout/layoutConfig.ts
@@ -1,11 +1,13 @@
 import type { Tab } from '@/client/types/layout';
 
+const homeTab = { tabname: 'Home', path: '/' };
 const signInTab = { tabname: 'Sign In', path: '/sign-in' };
 const signUpTab = { tabname: 'Start Growing', path: '/sign-up' };
-const exploreTab = { tabname: 'Explore', path: '/generate' }; // tbd
-const visualizeTab = { tabname: 'Visualize', path: '/output' }; // tbd
+const exploreTab = { tabname: 'Explore', path: '/generate' };
+const visualizeTab = { tabname: 'Visualize', path: '/output' };
 
 export const topNavigationTabs: Record<string, Tab[]> = {
   '/': [signInTab, signUpTab, exploreTab, visualizeTab],
-  // add more tabs here
+  '/generate': [homeTab, visualizeTab],
+  '/output': [homeTab, exploreTab],
 };

416ba92e4b6ee62570b1531c7d1bed13169ab813
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-25 13:42:03 -0500

    refactor var names

--- client/components/layout/main-layout/layoutConfig.ts
+++ client/components/layout/main-layout/layoutConfig.ts
@@ -1,18 +1,18 @@
 import type { Tab } from '@/client/types/layout';
 /**
- * Tab objects representing different route.
+ * Tab objects representing different routes.
  */
 const homeTab = { tabname: 'Home', path: '/' };
 const signInTab = { tabname: 'Sign In', path: '/sign-in' };
-const signUpTab = { tabname: 'Start Growing', path: '/sign-up' };
-const exploreTab = { tabname: 'Explore', path: '/generate' };
+const signUpTab = { tabname: 'Sign Up', path: '/sign-up' };
+const buildTreeTab = { tabname: 'Build Your Tree', path: '/generate' };
 const visualizeTab = { tabname: 'Visualize', path: '/output' };
 /**
  * Object mapping routes to their corresponding navigation tabs.
  * Each key represents a route, and the value is an array of Tab objects.
  */
 export const topNavigationTabsByRoute: Record<string, Tab[]> = {
-  '/': [signInTab, signUpTab, exploreTab, visualizeTab],
+  '/': [signInTab, signUpTab, buildTreeTab],
   '/generate': [homeTab, visualizeTab],
-  '/output': [homeTab, exploreTab],
+  '/output': [homeTab, buildTreeTab],
 };

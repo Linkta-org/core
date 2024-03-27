3772bd5610394232a583515cd14dc97b9aff326f
zhiyi15 <zhiyihuang1994@gmail.com>
2024-03-27 13:29:02 -0700

    fix: check apiKey before assigning into firebase config

--- client/config/firebaseWeb.ts
+++ client/config/firebaseWeb.ts
@@ -10,17 +10,18 @@ interface FirebaseConfig {
   appId: string | undefined;
 }
 
-const firebaseConfig: FirebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};
-
 let auth: Auth;
-if (firebaseConfig.apiKey) {
+
+if (process.env.FIREBASE_API_KEY) {
+  const firebaseConfig: FirebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+  };
+
   const firebaseApp = initializeApp(firebaseConfig);
   auth = getAuth(firebaseApp);
 } else {

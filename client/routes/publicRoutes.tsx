05781dc7bba449f07c70c8f21690b7a30ca78a2f
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-26 21:42:35 -0500

    update public routes

--- client/routes/publicRoutes.tsx
+++ client/routes/publicRoutes.tsx
@@ -1,31 +1,15 @@
 import React from 'react';
-import MainLayout from '@/client/components/layout/main-layout/MainLayout';
-import PromptInputPage from '@/client/features/prompt-input-page/PromptInputPage';
-import OutputVisualizationPage from '@/client/features/output-visualization-page/OutputVisualizationPage';
 import type { RouteObject } from 'react-router-dom';
-import NotFoundPage from '@/client/features/not-found-page/NotFoundPage';
-
 /**
  * The publicRoutes variable is an array which contains objects that each represent a route.
  * Our path property is set to the default route. Setting the element property to the MainLayout
  * component at the top level will allow its children to render MainLayout.
  *
  */
-
 const publicRoutes: RouteObject[] = [
-  {
-    path: '/',
-    element: <MainLayout />,
-    children: [
-      { path: '/generate', element: <PromptInputPage /> },
-      { path: '/output', element: <OutputVisualizationPage /> },
-    ],
-  },
-  // here we implement a catch-all route handler that renders the NotFoundPage component for unknown routes
-  {
-    path: '*',
-    element: <NotFoundPage />,
-  },
+  { index: true, element: <>HomePage placeholder</> },
+  { path: 'sign-in', element: <>SignInPage placeholder</> },
+  { path: 'sign-in', element: <>SignUpPage placeholder</> },
 ];
 
 export default publicRoutes;

a75204473daa19a0e4202c040711d9ece8d070cc
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-26 19:56:37 -0500

    add error handling

--- client/routes/ProtectedRoute.tsx
+++ client/routes/ProtectedRoute.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import Loader from '../components/common/Loader';
+import Loader from '@/client/components/common/Loader';
 
 const ProtectedRoute = () => {
   const isAuthenticated = true; //TODO: replace with authentication logic
   const isLoading = true; // TODO: replace with loading logic
+  const error = ''; // TODO: implement error handling logic
 
-  if(isLoading){
+  if (isLoading) {
     return <Loader />;
   }
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/" replace={true} />;
+  return (
+    <div>
+      {error && <p>error message placeholder</p>}
+      {isAuthenticated ? (
+        <Outlet />
+      ) : (
+        <Navigate
+          to="/"
+          replace={true}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ProtectedRoute;

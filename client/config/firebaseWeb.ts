77b98a7e4ea3a145e77dbbd61066ac18eeeaaf45
zhiyi15 <zhiyihuang1994@gmail.com>
2024-03-26 17:01:47 -0700

    fix: added interface for firebaseConfig

--- client/config/firebaseWeb.ts
+++ client/config/firebaseWeb.ts
@@ -1,8 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
+interface FirebaseConfig {
+  apiKey: string | undefined;
+  authDomain: string | undefined;
+  projectId: string | undefined;
+  storageBucket: string | undefined;
+  messagingSenderId: string | undefined;
+  appId: string | undefined;
+}
 // Linkta web app's Firebase configuration
-const firebaseConfig = {
+const firebaseConfig: FirebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,

edf5d259799d623f6bd424b33ddf146b8e008a9d
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-26 21:53:55 -0500

    add to-do

--- client/routes/publicRoutes.tsx
+++ client/routes/publicRoutes.tsx
@@ -3,6 +3,7 @@ import type { RouteObject } from 'react-router-dom';
 /**
  * Array of RouteObject for public access within the application.
  */
+//TODO: replace palceholders with components when ready
 const publicRoutes: RouteObject[] = [
   { index: true, element: <>HomePage placeholder</> },
   { path: 'sign-in', element: <>SignInPage placeholder</> },

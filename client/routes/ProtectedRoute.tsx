b3ae875bf3dfca9a34b9d79ca4a400b85c4017bb
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-26 19:54:38 -0500

    update navigate prop

--- client/routes/ProtectedRoute.tsx
+++ client/routes/ProtectedRoute.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import Loader from '../components/common/Loader';
 
 const ProtectedRoute = () => {
-  const isAuthenticated = true; //TODO: implement authentication logic here
+  const isAuthenticated = true; //TODO: replace with authentication logic
+  const isLoading = true; // TODO: replace with loading logic
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
+  if(isLoading){
+    return <Loader />;
+  }
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/" replace={true} />;
 };
 
 export default ProtectedRoute;

d530e9f5ac3d68178dda2c2bd85c9efcdd6f1014
Darren Pavel <darren.pavel@gmail.com>
2024-03-27 13:39:31 -0700

    Fix to remove explicit any.

--- server/utils/typeChecker.ts
+++ server/utils/typeChecker.ts
@@ -1,33 +1,17 @@
 /**
- * Generic type interface for use with isType.
- */
-interface Type {
-  [key: string]: any;
-}
-
-/**
  * Check if an object is of a certain type.
  *
- * @param object
- * @param type
- * @returns
+ * This function checks if an object is of a certain type by comparing the types of the object's properties to the types of the properties of the type object.
+ * Use by creating a new object of the type you want to check against and passing it as the second argument.
+ *
+ * @param object The object to check
+ * @param type An object of the the type to check against
+ * @return True if the object is of the type, false otherwise
  */
-export function isType(object: Type, type: Type): boolean {
-  const objectKeys = Object.keys(object);
-  const typeKeys = Object.keys(type);
-
-  if (objectKeys.length !== typeKeys.length) {
-    return false;
-  }
-
-  for (const key of objectKeys) {
-    if (
-      !typeKeys.hasOwnProperty(key) ||
-      typeof object[key] !== typeof type[key]
-    ) {
-      return false;
-    }
-  }
-
-  return true;
+export function isType<T extends object>(object: T, type: Partial<T>): boolean {
+  return (Object.keys(type) as (keyof T)[]).every((key: keyof T) => {
+    const objectType = typeof object[key];
+    const expectedType = typeof type[key];
+    return objectType === expectedType;
+  });
 }

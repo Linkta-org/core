8e955f31e7e0432a99609f444c65a302d220c307
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-26 21:10:31 -0500

    add comments

--- client/hooks/useAuth.ts
+++ client/hooks/useAuth.ts
@@ -1,11 +1,19 @@
 import { useState } from 'react';
-// TODO: implement auth logic
+/**
+ * Custom hook to manage authentication state. It initializes with an unauthenticated state,
+ * loading status, and no errors.
+ *
+ * @returns {object} - An object containing:
+ *   - `isAuthenticated`: Boolean indicating whether the user is currently authenticated.
+ *   - `isLoading`: Boolean indicating if the authentication status check is in progress.
+ *   - `error`: String for any authentication error messages or `null` if there are no errors.
+ */
 const useAuth = () => {
-  const [isAuthenticated, setIsAUthenticated] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isAuthenticated, setIsAUthenticated] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
-  // implement auth logic here
+  // TODO: implement auth logic here
 
   return { isAuthenticated, isLoading, error };
 };
--- client/routes/ProtectedRoute.tsx
+++ client/routes/ProtectedRoute.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import Loader from '@/client/components/common/Loader';
-import useAuth from '../hooks/useAuth';
-
+import useAuth from '@/client/hooks/useAuth';
+/**
+ * A higher-order component that guards child routes, ensuring they are accessible only to authenticated users. It leverages the `useAuth` hook to determine the user's authentication status.
+ *
+ * Unauthenticated users are redirected to the sign-in page.
+ */
 const ProtectedRoute: React.FC = () => {
   const { isAuthenticated, isLoading, error } = useAuth();
 
   if (isLoading) {
     return <Loader />;
   }
-
+  // TODO: update erroPage when ready
   return (
     <div>
       {error && <div>errorPage placeholder</div>}

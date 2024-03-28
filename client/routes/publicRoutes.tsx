1947a6195f1a4f87ac72cc00f7e0b342435aa1b5
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-03-28 12:34:00 -0500

    added home page to public routes

--- client/routes/publicRoutes.tsx
+++ client/routes/publicRoutes.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import type { RouteObject } from 'react-router-dom';
+import HomePage from '../features/home-page/HomePage';
 /**
  * Array of RouteObject for public access within the application.
  */
 //TODO: replace palceholders with components when ready
 const publicRoutes: RouteObject[] = [
-  { index: true, element: <>HomePage placeholder</> },
+  { index: true, element: <HomePage /> },
   { path: 'sign-in', element: <>SignInPage placeholder</> },
   { path: 'sign-up', element: <>SignUpPage placeholder</> },
 ];

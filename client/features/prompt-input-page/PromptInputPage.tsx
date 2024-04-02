be5d4478c77d35dfc5f8cc8cde10f4a969a72742
ejwoods <ewoods08@gmail.com>
2024-04-02 13:10:38 -0400

    import PromptInputForm to PromptInputPage, update PromptInputPage placeholder element

--- client/features/prompt-input-page/PromptInputPage.tsx
+++ client/features/prompt-input-page/PromptInputPage.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import PromptInputForm from '../../components/common/PromptInputForm';
 
 const PromptInputPage = () => {
-  return <div>PromptInputPage Placeholder</div>;
+  return (
+    <>
+      <div>PromptInputPage Placeholder:</div>
+      <div>Radix Form Element Placeholder</div>
+      <PromptInputForm />
+    </>
+  );
 };
 
 export default PromptInputPage;

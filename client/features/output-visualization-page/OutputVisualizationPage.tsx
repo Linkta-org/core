691367f312ff1f6edbae2e4c0575eab2125dbe37
Andrew Lo <andrewlo957@gmail.com>
2024-04-03 03:04:17 -1000

    imported PromptInputPage and TreeVisualizationBox

--- client/features/output-visualization-page/OutputVisualizationPage.tsx
+++ client/features/output-visualization-page/OutputVisualizationPage.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import PromptInputPage from '@/client/features/prompt-input-page/PromptInputPage';
+import TreeVisualizationBox from './components/TreeVisualizationBox';
 
-const OutputVisualizationPage = () => {
-  return <div>OutputVisualizationPage Placeholder</div>;
+const OutputVisualizationPage: React.FC = () => {
+  return (
+    <div>
+      <PromptInputPage />
+      <TreeVisualizationBox />
+    </div>
+  );
 };
 
 export default OutputVisualizationPage;

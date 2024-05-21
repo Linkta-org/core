22df56690b1604663e1e85b03d43f39321311c64
AaronDreyfuss <AaronDreyfuss@gmail.com>
2024-05-21 10:15:06 -0400

    Define LinktaFlow  model

--- server/types/datamodels.d.ts
+++ server/types/datamodels.d.ts
@@ -14,6 +14,77 @@ export type UserInput = {
   updatedAt: Date;
 };
 
+export type LinktaFlow = {
+  _id: Types.ObjectId;
+  userId: Types.ObjectId;
+  nodes: Node[];
+  edges: Edge[];
+  userInputId: Types.ObjectId;
+};
+
+export type Node = {
+  _id: Types.ObjectId;
+  type: string;
+  position: {
+    x: number;
+    y: number;
+  };
+  data: {
+    label: string;
+  };
+  style?: object;
+  className?: string;
+  hidden?: boolean;
+  selected?: boolean;
+  draggable?: boolean;
+  dragging?: boolean;
+  selectable?: boolean;
+  connectable?: boolean;
+  deletable?: boolean;
+  dragHandle?: string;
+  width?: number;
+  height?: number;
+  parentId?: string;
+  zIndex?: number;
+  extent?: 'parent' | null;
+  expandParent?: boolean;
+  sourcePosition?: 'left' | 'right' | 'top' | 'bottom';
+  targetPosition?: 'left' | 'right' | 'top' | 'bottom';
+  ariaLabel?: string;
+  focusable?: boolean;
+  resizing?: boolean;
+};
+
+export type Edge = {
+  _id: Types.ObjectId;
+  source: string;
+  target: string;
+  type?: string;
+  sourceHandle?: string;
+  targetHandle?: string;
+  style?: object;
+  animated?: boolean;
+  hidden?: boolean;
+  deletable?: boolean;
+  className?: string;
+  selected?: boolean;
+  zIndex?: number;
+  ariaLabel?: string;
+  focusable?: boolean;
+  label?: string;
+  labelStyle?: object;
+  labelShowBg?: boolean;
+  labelBgStyle?: object;
+  labelBgPadding?: number[];
+  labelBgBorderRadius?: number;
+  pathOptions?: {
+    offset?: number;
+    borderRadius?: number;
+    curvature?: number;
+  };
+};
+
+
 export type TreeNode = {
   _id: Types.ObjectId;
   content: string;

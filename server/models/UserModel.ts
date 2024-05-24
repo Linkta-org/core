55300edd028a4f78db687e3bd68e6d55e8b3b0a4
AaronDreyfuss <AaronDreyfuss@gmail.com>
2024-05-24 15:19:05 -0400

    Delete treeNode schema

--- server/models/UserModel.ts
+++ server/models/UserModel.ts
@@ -3,7 +3,7 @@ import type { User } from '@/server/types/datamodels';
 
 const userSchema = new Schema<User>({
   firstName: { type: String, required: true, minlength: 3, maxlength: 30, trim: true },
-  lastName: { type: String },
+  lastName: { type: String, required: true },
   userInputs: [{ type: Types.ObjectId, ref: 'UserInput' }],
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },

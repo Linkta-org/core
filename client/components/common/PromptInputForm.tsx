3911806e9fb49780642f4fe13459db88b24a014e
Andrew Lo <andrewlo957@gmail.com>
2024-05-23 21:30:39 -0700

    create UserInputBar component with mui styled api

--- client/components/common/PromptInputForm.tsx
+++ client/components/common/PromptInputForm.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import type { TextFieldProps } from '@mui/material';
 import { TextField } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const UserInputBar = styled(TextField)<TextFieldProps>(({ theme }) => ({
+  width: 500,
+  color: theme.palette.secondary.main,
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: theme.palette.secondary.main,
+    },
+    '& input': {
+      backgroundColor: theme.palette.grey[200],
+    },
+  },
+}));
 
 const PromptInputForm = () => {
   return (
     <div>
-      <TextField
-        id="user-input"
-        variant="outlined"
-        color="secondary"
-        sx={{
-          width: 500,
-        }}
-      />
+      <UserInputBar />
     </div>
   );
 };

b767193ed6b18aa8e6c5672dab27a56e351e4819
Andrew Lo <andrewlo957@gmail.com>
2024-05-23 21:46:35 -0700

    create button component with mui styled api

--- client/components/common/PromptInputForm.tsx
+++ client/components/common/PromptInputForm.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import type { TextFieldProps } from '@mui/material';
-import { TextField } from '@mui/material';
+import type { TextFieldProps, ButtonProps } from '@mui/material';
+import { TextField, Button } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 const UserInputBar = styled(TextField)<TextFieldProps>(({ theme }) => ({
@@ -16,11 +16,20 @@ const UserInputBar = styled(TextField)<TextFieldProps>(({ theme }) => ({
   },
 }));
 
+const GenerateButton = styled(Button)<ButtonProps>(({ theme }) => ({
+  height: 30,
+  width: 150,
+  color: 'black',
+  backgroundColor: theme.palette.secondary.main,
+  textTransform: 'none',
+}));
+
 const PromptInputForm = () => {
   return (
     <div>
       <h2>Start your learning journey here:</h2>
       <UserInputBar />
+      <GenerateButton>Generate</GenerateButton>
     </div>
   );
 };

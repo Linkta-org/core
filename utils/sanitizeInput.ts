73ff171ca61e64923ae87d34a923793b5d9704ca
zhiyi15 <zhiyihuang1994@gmail.com>
2024-05-30 12:34:30 -0700

    added validation and sanitization code from old PR

---
+++ utils/sanitizeInput.ts
@@ -0,0 +1,34 @@
+import { z } from 'zod';
+
+/**
+ * Trims and replaces multiple whitespace characters with a single space.
+ * @param {string} input - The string to process.
+ * @returns {string} The processed string.
+ */
+const removeExtraWhiteSpaces = (input: string): string => {
+  return input.trim().replace(/\s+/g, ' ');
+};
+
+/**
+ * Escapes HTML special characters in a string.
+ * @param {string} input - The string to process.
+ * @returns {string} The processed string with escaped HTML characters.
+ */
+
+const escapeHTML = (input: string): string => {
+  return input
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;')
+    .replace(/\//g, '&#x2F;');
+};
+
+const userInputSanitizationSchema = z.object({
+  input: z.string().transform(removeExtraWhiteSpaces).transform(escapeHTML),
+});
+
+export type UserInputSanitization = z.infer<typeof userInputSanitizationSchema>;
+
+export default userInputSanitizationSchema;

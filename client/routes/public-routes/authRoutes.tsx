56b1bfea83c1f78f4d0d9c77bbf6f38bc79d9868
Andrew Lo <andrewlo957@gmail.com>
2024-05-30 16:31:17 -0700

    add routes to authRoutes

--- client/routes/public-routes/authRoutes.tsx
+++ client/routes/public-routes/authRoutes.tsx
@@ -1,3 +1,5 @@
+import HelpAndFeedback from '@/client/features/help-and-feedback-page/HelpAndFeedbackPage';
+import Settings from '@/client/features/settings-page/Settings';
 import React from 'react';
 import type { RouteObject } from 'react-router-dom';
 //TODO: replace palceholders with components when ready
@@ -6,6 +8,8 @@ const authRoutes: RouteObject[] = [
   { path: 'sign-up', element: <>SignUpPage placeholder</> },
   { path: 'forgot-pwd', element: <>ForgotPasswordPage placeholder</> },
   { path: 'reset-pwd', element: <>PasswordResetPage placeholder</> },
+  { path: 'help-and-feedback', element: <HelpAndFeedback /> },
+  { path: 'settings', element: <Settings /> },
 ];
 
 export default authRoutes;

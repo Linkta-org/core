ea72a62ac91dd11dfa97eb4922e142c2f469f787
relandboyle <relandboyle@users.noreply.github.com>
2024-05-31 19:36:50 +0000

    chore: format files from GitHub Action

--- client/store/AuthStore.ts
+++ client/store/AuthStore.ts
@@ -2,31 +2,34 @@ import { createStore } from 'zustand/vanilla';
 import { devtools } from 'zustand/middleware';
 
 type TokenDataSchema = {
-	userId: string,
-	roles: string[],
-}
+  userId: string;
+  roles: string[];
+};
 
 type AuthStore = {
-	accessToken: string | undefined;
-	refreshToken: string | undefined;
-	accessTokenData: TokenDataSchema | undefined;
-}
+  accessToken: string | undefined;
+  refreshToken: string | undefined;
+  accessTokenData: TokenDataSchema | undefined;
+};
 
-export const useAuthStore = createStore<AuthStore>()(devtools((set, get) => ({
-			accessToken: undefined,
-			refreshToken: undefined,
-			accessTokenData: undefined,
+export const useAuthStore = createStore<AuthStore>()(
+  devtools(
+    (set, get) => ({
+      accessToken: undefined,
+      refreshToken: undefined,
+      accessTokenData: undefined,
       setAccessToken: set((state) => ({ accessToken: state.accessToken })),
       setRefreshToken: set((state) => ({ refreshToken: state.refreshToken })),
-      setAccessTokenData: set((state) => ({ accessTokenData: state.accessTokenData })),
+      setAccessTokenData: set((state) => ({
+        accessTokenData: state.accessTokenData,
+      })),
       getAccessToken: get().accessToken,
       getRefreshToken: get().refreshToken,
-      getRefreshTokenData: get().accessTokenData
-		}),
-		{
-			name: 'auth-store',
-			enabled: true,
-		}
-	)
+      getRefreshTokenData: get().accessTokenData,
+    }),
+    {
+      name: 'auth-store',
+      enabled: true,
+    }
+  )
 );
-
--- client/store/FlowStore.ts
+++ client/store/FlowStore.ts
@@ -1,31 +1,36 @@
 import { createStore } from 'zustand/vanilla';
 import { devtools } from 'zustand/middleware';
-import type { LinktaFlow } from '../types/datamodels';
+import type { LinktaFlow } from '@/client/types/datamodels';
 
 type FlowStore = {
   flow: LinktaFlow | undefined;
   fetchFlow: () => Promise<void>;
   updateFlow: void;
-}
+};
 
-export const useFlowStore = createStore<FlowStore>()(devtools((set, get) => ({
-  flow: undefined,
-  getFlow: get().flow,
-  fetchFlow: async (): Promise<void> => {
-    await fetch('placeholder URL')
-      .then(res => res.json())
-      .then(data => {
-        set({ flow: data })
-      })
-      .catch((err) => {
-        console.error('An error occurred while fetching data from the server.', err);
-      })
-  },
-  updateFlow: set((state) => ({ flow: state.flow }))
-}),
-  {
-    name: 'flow-store',
-    enabled: true,
-  }
-));
-
+export const useFlowStore = createStore<FlowStore>()(
+  devtools(
+    (set, get) => ({
+      flow: undefined,
+      getFlow: get().flow,
+      fetchFlow: async (): Promise<void> => {
+        await fetch('placeholder URL')
+          .then((res) => res.json())
+          .then((data) => {
+            set({ flow: data });
+          })
+          .catch((err) => {
+            console.error(
+              'An error occurred while fetching data from the server.',
+              err
+            );
+          });
+      },
+      updateFlow: set((state) => ({ flow: state.flow })),
+    }),
+    {
+      name: 'flow-store',
+      enabled: true,
+    }
+  )
+);

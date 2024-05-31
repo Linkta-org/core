7737c02cec229f0e48e60e47842e6555905b95f0
Andrew Lo <andrewlo957@gmail.com>
2024-05-31 13:16:15 -0700

    fix export statements for auth pages; replace element placeholders with components

--- client/routes/public-routes/authRoutes.tsx
+++ client/routes/public-routes/authRoutes.tsx
@@ -1,13 +1,18 @@
-import HelpAndFeedback from '@/client/features/help-and-feedback-page/HelpAndFeedbackPage';
-import Settings from '@/client/features/settings-page/Settings';
 import React from 'react';
 import type { RouteObject } from 'react-router-dom';
+import HelpAndFeedback from '@/client/features/help-and-feedback-page/HelpAndFeedbackPage';
+import Settings from '@/client/features/settings-page/Settings';
+import SignInPage from '@/client/features/auth-pages/SignInPage';
+import SignUpPage from '@/client/features/auth-pages/SignUpPage';
+import ForgotPasswordPage from '@/client/features/auth-pages/ForgotPasswordPage';
+import PasswordResetPage from '@/client/features/auth-pages/PasswordResetPage';
+
 //TODO: replace palceholders with components when ready
 const authRoutes: RouteObject[] = [
-  { path: 'sign-in', element: <>SignInPage placeholder</> },
-  { path: 'sign-up', element: <>SignUpPage placeholder</> },
-  { path: 'forgot-pwd', element: <>ForgotPasswordPage placeholder</> },
-  { path: 'reset-pwd', element: <>PasswordResetPage placeholder</> },
+  { path: 'sign-in', element: <SignInPage /> },
+  { path: 'sign-up', element: <SignUpPage /> },
+  { path: 'forgot-pwd', element: <ForgotPasswordPage /> },
+  { path: 'reset-pwd', element: <PasswordResetPage /> },
   { path: 'help-and-feedback', element: <HelpAndFeedback /> },
   { path: 'settings', element: <Settings /> },
 ];

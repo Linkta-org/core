76d541e16f8b907427732fab038fc8eb6fe630d6
zhiyi15 <zhiyihuang1994@gmail.com>
2024-06-04 21:50:08 -0700

    added generateInitialResponse middleware to userInput route

--- server/routes/userInput.ts
+++ server/routes/userInput.ts
@@ -1,11 +1,20 @@
 import { Router } from 'express';
+import type { Request, Response } from 'express';
 import {
   storeUserInputDatabase,
   submitUserInput,
 } from '@server/controllers/userInputController';
+import { generateInitialResponse } from '@/server/controllers/genAiController';
 
 const router = Router();
 
-router.post('/', storeUserInputDatabase, submitUserInput);
+router.post(
+  '/',
+  storeUserInputDatabase,
+  submitUserInput,
+  generateInitialResponse,
+  (_: Request, res: Response) => {
+    res.send({ message: 'linktaFlow', response: res.locals.linktaFlow });
+});
 
 export default router;

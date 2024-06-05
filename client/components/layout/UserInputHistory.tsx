d8356998ae1857dd46c521bafa2a404d3c4e0205
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-06-05 18:38:11 -0500

    created simple show more feature

--- client/components/layout/UserInputHistory.tsx
+++ client/components/layout/UserInputHistory.tsx
@@ -1,29 +1,57 @@
-import React from 'react';
-import { Box, List, ListItem, ListItemText, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  Button,
+} from '@mui/material';
 import { MOCK_USER_INPUT_LIST } from '@/mocks';
 
+const MAX_HEIGHT = 1000;
+const ITEMS_PER_PAGE = 10;
+
 export default function UserInputHistory() {
+  const [visibleItems, setVisibleItems] = useState(ITEMS_PER_PAGE);
   const userInputs = MOCK_USER_INPUT_LIST;
+
+  const handleShowMore = () => {
+    setVisibleItems((prevVisibleItems) => prevVisibleItems + ITEMS_PER_PAGE);
+  };
 
   return (
     <Box
       className="recent-user-inputs"
       mt={5}
       pl={2}
+      sx={{ maxHeight: MAX_HEIGHT, overflow: 'auto' }}
     >
       <Typography
         variant="body2"
         color={'primary.contrastText'}
+        gutterBottom
       >
         Recent
       </Typography>
       <List>
-        {userInputs.map((input) => (
+        {userInputs.slice(0, visibleItems).map((input) => (
           <ListItem key={input._id}>
-            <ListItemText primary={input.input} />
+            <ListItemText
+              primary={<Typography variant="caption">{input.input}</Typography>}
+            />
           </ListItem>
         ))}
       </List>
+      {visibleItems < userInputs.length && (
+        <Button
+          onClick={handleShowMore}
+          variant="contained"
+          sx={{ mt: 2 }}
+        >
+          Show More
+        </Button>
+      )}
     </Box>
   );
 }

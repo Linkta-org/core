93f3381de186b3d9e59acbb37df18172c26b4cb7
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-06-05 22:16:47 -0500

    extracted user input list from user input history

---
+++ client/components/layout/UserInputList.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { List, ListItem, ListItemText, Typography } from '@mui/material';
+
+interface UserInput {
+  _id: string;
+  input: string;
+}
+
+interface UserInputListProps {
+  userInputList: UserInput[];
+  visibleItems: number;
+}
+
+export const UserInputList: React.FC<UserInputListProps> = ({
+  userInputList,
+  visibleItems,
+}) => {
+  return (
+    <List>
+      {userInputList.slice(0, visibleItems).map((input, index) => (
+        <ListItem key={`${input._id}-${index}`}>
+          <ListItemText
+            primary={
+              <Typography
+                variant="caption"
+                noWrap
+                sx={{
+                  display: 'inline-block',
+                  whiteSpace: 'nowrap',
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                  maxWidth: '100%',
+                }}
+              >
+                {input.input}
+              </Typography>
+            }
+          />
+        </ListItem>
+      ))}
+    </List>
+  );
+};

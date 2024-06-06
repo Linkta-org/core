b0ea19460a1a0932c52a8f343f0aca2e0d13d723
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-06-06 16:40:03 -0500

    added icon button

--- client/components/layout/UserInputList.tsx
+++ client/components/layout/UserInputList.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {
+  IconButton,
   List,
   ListItem,
   ListItemText,
   Typography,
   styled,
 } from '@mui/material';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 interface UserInput {
   _id: string;
@@ -28,14 +30,23 @@ const StyledListItem = styled(ListItem)({
 const StyledTypography = styled(Typography)({
   display: 'inline-block',
   overflow: 'hidden',
-  textOverflow: 'ellipsis',
+  textOverflow: 'clip',
   maxWidth: '100%',
+});
+
+const StyledIconButton = styled(IconButton)({
+  color: 'inherit',
+  transform: 'scale(0.6)',
 });
 
 const UserInputList: React.FC<UserInputListProps> = ({
   userInputList,
   visibleItems,
 }) => {
+  const handleIconClick = (id: string) => {
+    console.log(`Clicked on icon for item with id: ${id}`); // TODO: Implement more logic
+  };
+
   return (
     <StyledList role="list">
       {userInputList.slice(0, visibleItems).map((input, index) => (
@@ -56,6 +67,12 @@ const UserInputList: React.FC<UserInputListProps> = ({
               </StyledTypography>
             }
           />
+          <StyledIconButton
+            aria-label={`More options for ${input.input}`}
+            onClick={() => handleIconClick(input._id)}
+          >
+            <MoreHorizIcon />
+          </StyledIconButton>
         </StyledListItem>
       ))}
     </StyledList>

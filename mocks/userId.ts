51357e328f6e39cf2f83fe86682ed8ee97074366
zhiyi15 <zhiyihuang1994@gmail.com>
2024-06-07 11:30:11 -0700

    added comment; change name

--- mocks/userId.ts
+++ mocks/userId.ts
@@ -1,3 +1,4 @@
 export const MOCK_USER_ID = '60a5f5c1f8d2b12f4c8e1f4a';
 
-export const NEW_MOCK_USER_ID = '66625d317288b23e84786fe8';
+//mock_user_id_2 is an actual user document in db
+export const MOCK_USER_ID_2 = '66625d317288b23e84786fe8';

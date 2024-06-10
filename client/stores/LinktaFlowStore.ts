8819544e5437bb969fe0dd91f22a348563170111
relandboyle <relandboyle@users.noreply.github.com>
2024-06-10 18:17:36 +0000

    chore: format files from GitHub Action

--- client/stores/LinktaFlowStore.ts
+++ client/stores/LinktaFlowStore.ts
@@ -1,22 +1,23 @@
-import { create } from "zustand";
-import { LinktaFlow } from "../types/datamodels";
+import { create } from 'zustand';
+import type { LinktaFlow } from '@/client/types/datamodels';
 
 type LinktaFlowStore = {
-  currentLinktaFlow: LinktaFlow | undefined,
-  undoHistory: LinktaFlow[],
-  getCurrentFlow: () => LinktaFlow | undefined,
-  getUndoHistory: () => LinktaFlow[],
-  setCurrentFlow: () => void,
-  setUndoHistory: () => void
-}
+  currentLinktaFlow: LinktaFlow | undefined;
+  undoHistory: LinktaFlow[];
+  getCurrentFlow: () => LinktaFlow | undefined;
+  getUndoHistory: () => LinktaFlow[];
+  setCurrentFlow: () => void;
+  setUndoHistory: () => void;
+};
 
-const useLinktaFlowStore = create<LinktaFlowStore>() ((set, get) => ({
+const useLinktaFlowStore = create<LinktaFlowStore>()((set, get) => ({
   currentLinktaFlow: undefined,
   undoHistory: [],
   getCurrentFlow: () => get().currentLinktaFlow,
   getUndoHistory: () => get().undoHistory,
-  setCurrentFlow: () => set((state) => ({ currentLinktaFlow: state.currentLinktaFlow })),
-  setUndoHistory: () => set((state) => ({ undoHistory: state.undoHistory}))
-}))
+  setCurrentFlow: () =>
+    set((state) => ({ currentLinktaFlow: state.currentLinktaFlow })),
+  setUndoHistory: () => set((state) => ({ undoHistory: state.undoHistory })),
+}));
 
 export default useLinktaFlowStore;
--- client/stores/UserInputStore.ts
+++ client/stores/UserInputStore.ts
@@ -1,22 +1,22 @@
 import { create } from 'zustand';
-import type { UserInput } from '../types/datamodels';
+import type { UserInput } from '@/client/types/datamodels';
 
 type userInputStore = {
-  lastInput: UserInput | undefined,
-  inputHistory: UserInput[],
-  getLastInput: () => UserInput | undefined,
-  getInputHistory: () => UserInput[],
-  setLastInput: () => void,
-  setInputHistory: () => void
-}
+  lastInput: UserInput | undefined;
+  inputHistory: UserInput[];
+  getLastInput: () => UserInput | undefined;
+  getInputHistory: () => UserInput[];
+  setLastInput: () => void;
+  setInputHistory: () => void;
+};
 
-const useUserInputStore = create<userInputStore>() ((set, get) => ({
+const useUserInputStore = create<userInputStore>()((set, get) => ({
   lastInput: undefined,
   inputHistory: [],
   getLastInput: () => get().lastInput,
   getInputHistory: () => get().inputHistory,
   setLastInput: () => set((state) => ({ lastInput: state.lastInput })),
-  setInputHistory: () => set((state) => ({ inputHistory: state.inputHistory  }))
-}))
+  setInputHistory: () => set((state) => ({ inputHistory: state.inputHistory })),
+}));
 
 export default useUserInputStore;

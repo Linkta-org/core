ea14c1f0d911d3ff707bdc76eb18ec8c968c9a42
Reland <reland.boyle@gmail.com>
2024-06-10 14:14:10 -0400

    Simple Zustand stores for UserInput and LinktaFlow

---
+++ client/stores/LinktaFlowStore.ts
@@ -0,0 +1,22 @@
+import { create } from "zustand";
+import { LinktaFlow } from "../types/datamodels";
+
+type LinktaFlowStore = {
+  currentLinktaFlow: LinktaFlow | undefined,
+  undoHistory: LinktaFlow[],
+  getCurrentFlow: () => LinktaFlow | undefined,
+  getUndoHistory: () => LinktaFlow[],
+  setCurrentFlow: () => void,
+  setUndoHistory: () => void
+}
+
+const useLinktaFlowStore = create<LinktaFlowStore>() ((set, get) => ({
+  currentLinktaFlow: undefined,
+  undoHistory: [],
+  getCurrentFlow: () => get().currentLinktaFlow,
+  getUndoHistory: () => get().undoHistory,
+  setCurrentFlow: () => set((state) => ({ currentLinktaFlow: state.currentLinktaFlow })),
+  setUndoHistory: () => set((state) => ({ undoHistory: state.undoHistory}))
+}))
+
+export default useLinktaFlowStore;
---
+++ client/stores/UserInputStore.ts
@@ -0,0 +1,22 @@
+import { create } from 'zustand';
+import type { UserInput } from '../types/datamodels';
+
+type userInputStore = {
+  lastInput: UserInput | undefined,
+  inputHistory: UserInput[],
+  getLastInput: () => UserInput | undefined,
+  getInputHistory: () => UserInput[],
+  setLastInput: () => void,
+  setInputHistory: () => void
+}
+
+const useUserInputStore = create<userInputStore>() ((set, get) => ({
+  lastInput: undefined,
+  inputHistory: [],
+  getLastInput: () => get().lastInput,
+  getInputHistory: () => get().inputHistory,
+  setLastInput: () => set((state) => ({ lastInput: state.lastInput })),
+  setInputHistory: () => set((state) => ({ inputHistory: state.inputHistory  }))
+}))
+
+export default useUserInputStore;

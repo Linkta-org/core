ab5bddb914c0a2bda70f4d209c30236b5e7083f9
Reland <reland.boyle@gmail.com>
2024-06-11 07:48:12 -0400

    Added Turbo Console; Added Vite Plugin Checker; removed ENV varibales from npm scripts

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import TurboConsole from 'unplugin-turbo-console/vite';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
   plugins: [
     react(), // React plugin for Vite
     tsconfigPaths(), // Resolve imports using TypeScript's path mapping
+    TurboConsole({
+      prefix: `[Linkta]`,
+      disableLaunchEditor: true,
+    }),
+    checker({
+      typescript: true,
+      overlay: false,
+    })
   ],
   root: path.resolve(__dirname), // Define root directory for the Vite project.
   server: {

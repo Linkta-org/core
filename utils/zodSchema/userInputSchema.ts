2d5a2ea62ff4e8374347fdd924a79071fdf30a42
Andrew Lo <andrewlo957@gmail.com>
2024-06-12 00:22:24 -0700

    remove userInputSchema

--- utils/zodSchema/userInputSchema.ts
+++
@@ -1,8 +0,0 @@
-import userInputSanitizationSchema from '@/utils/sanitizeInput';
-import userInputValidationSchema from './userInputValidation';
-
-const userInputSchema = userInputSanitizationSchema.merge(
-  userInputValidationSchema
-);
-
-export default userInputSchema;

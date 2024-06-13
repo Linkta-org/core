5ae0837137e5b530ce0be6009fd7534a4ace61e3
Brian Preston <bpreston14@gmail.com>
2024-06-13 18:47:07 -0400

    create useDocumentTitle hook and add it to all current pages with routes

--- client/features/home-page/HomePage.tsx
+++ client/features/home-page/HomePage.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import HomePageMainHero from './components/HomePageMainHero';
 import HomePageTreeVisualizationPanel from './components/HomePageTreeVisualizationPanel';
+import useDocumentTitle from '@/client/hooks/useDocumentTitle';
 
 const HomePage = () => {
+  useDocumentTitle();
   return (
     <>
       <HomePageMainHero />

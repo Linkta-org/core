745796777935834d1c9ea3619ec82a69eb7b75df
Reland <reland.boyle@gmail.com>
2024-06-26 13:04:33 -0400

    In progress; HomePage layout

--- client/features/home-page/HomePage.tsx
+++ client/features/home-page/HomePage.tsx
@@ -7,25 +7,25 @@ const HomePage = () => {
   useDocumentTitle();
   return (
     <Box className={`${styles.homeContainer}`}>
-      <Typography variant='h4'>
-        Revolutionizing Learning: Intuitive Visualization for Complex Concepts
-      </Typography>
-      <Typography
-        variant='body2'
-        className={`${styles.bodyText}`}
-      >
-        Experience a new era of understanding, where concepts become clear and
-        logic flows effortlessly, making the path from confusion to clarity both
-        intuitive and accessible.
-      </Typography>
-      <Box className={`${styles.imagePlaceholder}`}>
+      <Box className={`${styles.copyContainer}`}>
+        <Typography variant='h4'>
+          Revolutionizing Learning: Intuitive Visualization for Complex Concepts
+        </Typography>
         <Typography
-          variant='h6'
-          color='secondary'
+          variant='body2'
+          className={`${styles.bodyText}`}
         >
-          Placeholder
+          Experience a new era of understanding, where concepts become clear and
+          logic flows effortlessly, making the path from confusion to clarity both
+          intuitive and accessible.
         </Typography>
       </Box>
+      <img
+        className={`${styles.homeImage}`}
+        alt='A static image of a 3D model showing various gadgets and widgets.'
+        src='../../assets/Linkta-Landing.png'
+      >
+      </img>
     </Box>
   );
 };

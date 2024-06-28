09a61d52ceb7c02f91a6fde80ce7a37e482b4b3b
Jiaxin (Kinski) Wu <kinskywu@gmail.com>
2024-06-28 18:41:55 -0500

    updated router

--- client/routes/publicRoutes.tsx
+++ client/routes/publicRoutes.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import type { RouteObject } from 'react-router-dom';
 import HomePage from '@features/home-page/HomePage';
-import unprotectedRoutes from '@routes/unprotectedRoutes';
+import SignInPage from '@features/auth-pages/SignInPage';
+import SignUpPage from '@features/auth-pages/SignUpPage';
+import ForgotPasswordPage from '@features/auth-pages/ForgotPasswordPage';
+import PasswordUpdatePage from '@features/auth-pages/PasswordUpdatePage';
+
 /**
  * Array of RouteObject for public access within the application.
  */
 const publicRoutes: RouteObject[] = [
   { index: true, element: <HomePage /> },
-  ...unprotectedRoutes,
+  { path: 'login', element: <SignInPage /> },
+  { path: 'signup', element: <SignUpPage /> },
+  { path: 'forgot-password', element: <ForgotPasswordPage /> },
+  { path: 'reset-password', element: <PasswordUpdatePage /> },
 ];
 
 export default publicRoutes;

5b6b4a202b2652097478844b178ab3626e3e06af
Colin <colin.vandergraaf@gmail.com>
2024-08-01 08:16:33 -0700

    Update NotFoundPage unit test

--- client/tests/unit/NotFoundPage.unit.test.tsx
+++ client/tests/unit/NotFoundPage.unit.test.tsx
@@ -3,23 +3,25 @@ import NotFoundPage from '@features/error-pages/NotFoundPage';
 import React from 'react';
 
 describe('NotFoundPage Component', async () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
   it('should render the "Page could not be found" message', async () => {
     render(<NotFoundPage />, { initialEntries: ['/non-existing-route'] });
-    expect(
-      await screen.findByText(/Page could not be found/),
-    ).toBeInTheDocument();
+    expect(screen.getByText(/Page could not be found/)).toBeInTheDocument();
   });
 
   it('should render the "Oopsie! Looks like the page got lost" message', async () => {
     render(<NotFoundPage />, { initialEntries: ['/non-existing-route'] });
     expect(
-      await screen.findByText(/Oopsie! Looks like the page got lost/),
+      screen.getByText(/Oopsie! Looks like the page got lost/),
     ).toBeInTheDocument();
   });
 
   it('should render the "404" message', async () => {
     render(<NotFoundPage />, { initialEntries: ['/non-existing-route'] });
-    expect(await screen.findByText(/404/)).toBeInTheDocument();
+    expect(screen.getByText(/404/)).toBeInTheDocument();
   });
 
   it('should navigate to the homepage when "Return to Homepage" button is clicked', async () => {

3f31d5719acac856137c5633f45e02af60456bb9
LucieSeidler <lucib0506@gmail.com>
2024-08-05 16:01:20 -0400

    commit initial styling

--- client/features/output-visualization-page/components/LinktaFlowNode.tsx
+++ client/features/output-visualization-page/components/LinktaFlowNode.tsx
@@ -2,7 +2,7 @@ import React, { useState, memo } from 'react';
 import type { NodeProps } from 'reactflow';
 import { Handle, Position } from 'reactflow';
 import { Box, TextField, Typography } from '@mui/material';
-import '@styles/react-flow.css';
+import styles from '@styles/layout/LinktaNode.module.css';
 
 type LinktaNodeData = {
   color?: string | undefined;
@@ -30,38 +30,41 @@ const LinktaFlowNode = memo(({ isConnectable, data }: LinktaNodeProps) => {
 
   return (
     <Box
+      className={styles.nodeBox}
       onClick={onFocus}
       onBlur={onBlur}
-      sx={{
-        bgcolor: isEditing ? '#1a3e37' : '#183636',
-        height: '44px',
-        width: '168px',
-        display: 'flex',
-        borderRadius: '5px',
-        border: isEditing ? '0.2px solid #FFA51B' : '0.2px  solid #000',
-        borderLeft: isEditing ? '0.2px solid #FFA51B' : 'none',
-        boxShadow: isEditing
-          ? '0px 0px 4px 1px #FFA51B96'
-          : '2px 4px 8px hsl(0deg 0% 0% / 0.25)',
-        transition: 'box-shadow 0.3s, border 0.3s',
-      }}
-      className='linkta-node'
+      // sx={{
+      //   bgcolor: isEditing ? '#1a3e37' : '#183636',
+      //   height: '44px',
+      //   width: '168px',
+      //   display: 'flex',
+      //   borderRadius: '5px',
+      //   border: isEditing ? '0.2px solid #FFA51B' : '0.2px  solid #000',
+      //   borderLeft: isEditing ? '0.2px solid #FFA51B' : 'none',
+      //   boxShadow: isEditing
+      //     ? '0px 0px 4px 1px #FFA51B96'
+      //     : '2px 4px 8px hsl(0deg 0% 0% / 0.25)',
+      //   transition: 'box-shadow 0.3s, border 0.3s',
+      // }}
+      // className='linkta-node'
     >
       <Box
-        sx={{
-          borderRadius: '5px 0 0 5px',
-          width: '8px',
-          bgcolor: data.color || '#0442E1',
-          height: '100%',
-        }}
+        className={styles.nodeHandle}
+        // sx={{
+        //   borderRadius: '5px 0 0 5px',
+        //   width: '8px',
+        //   bgcolor: data.color || '#0442E1',
+        //   height: '100%',
+        // }}
       />
       <Box
-        sx={{
-          flexGrow: 1,
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
+        className={styles.nodeTextBox}
+        // sx={{
+        //   flexGrow: 1,
+        //   display: 'flex',
+        //   alignItems: 'center',
+        //   justifyContent: 'center',
+        // }}
       >
         {isEditing && (
           <TextField
@@ -75,34 +78,36 @@ const LinktaFlowNode = memo(({ isConnectable, data }: LinktaNodeProps) => {
             value={data.label}
             id='filled-hidden-label-small'
             size='small'
-            sx={{
-              background: 'transparent',
-              border: 'none',
-              borderRadius: '1px',
-              color: 'inherit',
-              fontFamily: 'Inter, sans-serif',
-              fontWeight: 500,
-              padding: '0 2px',
+            // sx={{
+            //   background: 'transparent',
+            //   border: 'none',
+            //   borderRadius: '1px',
+            //   color: 'inherit',
+            //   fontFamily: 'Inter, sans-serif',
+            //   fontWeight: 500,
+            //   padding: '0 2px',
 
-              '& .MuiOutlinedInput-root': {
-                '& fieldset': {
-                  border: 'none',
-                },
-                '&.Mui-focused fieldset': {
-                  border: 'none',
-                },
-              },
-              '& input': {
-                textAlign: 'center',
-                fontSize: '11px',
-              },
-            }}
+            //   '& .MuiOutlinedInput-root': {
+            //     '& fieldset': {
+            //       border: 'none',
+            //     },
+            //     '&.Mui-focused fieldset': {
+            //       border: 'none',
+            //     },
+            //   },
+            //   '& input': {
+            //     textAlign: 'center',
+            //     fontSize: '11px',
+            //   },
+            // }}
+            className={styles.nodeTextField}
           />
         )}
         {!isEditing && (
           <Typography
             variant='body1'
             id='filled-hidden-label-small'
+            className={styles.nodeTypography}
             // size='small'
             // sx={{
             //   background: 'transparent',
